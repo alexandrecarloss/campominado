dd98ac5d50f05ddc21020567fb230a28e4611f7e
alexandrecarloss <107065086+alexandrecarloss@users.noreply.github.com>
2023-01-24 20:43:09 -0400

    Projeto concluído

--- index.js
+++ index.js
@@ -33,6 +33,19 @@ function field(rows_count, cols_count, mines) {
     return rows;
 }
 
+function clicou(event) {
+    if (event.target.textContent === '*') {
+        for (element of document.querySelectorAll('span')) {
+            element.setAttribute('class', '')
+        }
+        alert('perdeu playboy!');
+        window.location.reload();
+    } else {
+        event.target.childNodes[0].setAttribute('class','');
+    }
+    
+}
+
 function drawTable(rows) {
     var table = document.getElementById('field');
     for (row of rows) {
@@ -40,11 +53,28 @@ function drawTable(rows) {
         table.appendChild(tr);
         for (col of row) {
             var td = document.createElement('td');
-            td.textContent = col;
-            tr.appendChild(td)
+            var span = document.createElement('span');
+            span.textContent = col;
+            span.setAttribute('class', 'invisible');
+            td.appendChild(span);
+            tr.appendChild(td);
+            td.addEventListener('click', clicou);
         }       
     }
 }
 
-    var mines = [[0,7], [3,3], [5,2], [7,7], [4,2], [1,2]];
-    drawTable(field(8, 8, mines));
+function randomMines(quantity, cols, rows) {
+    mines = [];
+    for (var i = 0; i< quantity; i++) {
+        var positionRow = parseInt(Math.random() * rows);
+        var positionCol = parseInt(Math.random() * cols);
+        mines. push([positionRow, positionCol]);
+    }
+    return mines;
+}
+
+var mines =  randomMines(10, 8, 8); //[[0,7], [3,3], [5,2], [7,7], [4,2], [1,2]];
+var myField = (field(8, 8, mines));
+drawTable(myField);
+
+
